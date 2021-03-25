0c20734d0e990e4f67312daac385280f78fa89f6
serdf3r <serdf3r@hotmail.it>
2021-03-25 12:49:23 +0100

    only first page

--- models/Customer.php
+++ models/Customer.php
@@ -1,9 +1,17 @@
 <?php
 
-class Customer
-{
-    public function getTransactions()
-    {
-        
+class Customer {
+
+    public function getDate() {
+        echo "DATA TODO";
     }
+
+    public function getTransaction() {
+           echo "Transazione TODO";
+    }
+
+    public function getTransactions() {
+           echo "Transazioni TODO";
+    }
+
 }
--- scripts/report.php
+++ scripts/report.php
@@ -1,7 +1,14 @@
-<?php 
+<?php
 
-//TODO print formatted report
-
+require('./models/Customer.php');
+// --- $argv Contains an array of all the arguments passed to the script when running from the command line
+if (count($argv) != 3) {
+    throw new Exception("Gli argomenti passati devono essere Id-Cliente e Valuta di Coversione");
+}
+//print_r($argv);
+$customer = $argv[1];
+echo "Transazioni dell' utente " . $customer . "\n";
+$customer = new Customer(intval($argv[1]));
 foreach ($customer->getTransactions() as $transaction) {
-    
+    echo "In data " . $transaction->getDate() . " l'utente " . $customer . " ha fatto la seguente transazione: " . $transaction->getTransaction() . " --> " . $transaction->getTransaction() . "\n";
 }
